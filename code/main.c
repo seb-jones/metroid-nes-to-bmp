3b7aa9252b37e6fb6399f06bd39db118e7eba9b6
Seb Jones <jones.seb95@googlemail.com>
2022-09-19 21:27:18 +0100

    Fix incorrect palettes being used

--- code/main.c
+++ code/main.c
@@ -12,9 +12,11 @@
 #define MAP_HEIGHT 32
 #define IMAGE_WIDTH (MAP_WIDTH * SCREEN_WIDTH)
 #define IMAGE_HEIGHT (MAP_HEIGHT * SCREEN_HEIGHT)
+#define PALETTE_BYTES (32 * 4)
 
 int write_room_pixels_to_map(unsigned char *map_pixels, int map_x, int map_y);
-int write_map_pixels_to_image(unsigned char *map_pixels);
+int write_map_pixels_to_image(unsigned char *map_pixels,
+                              unsigned char *map_palettes);
 
 int main()
 {
@@ -27,15 +29,26 @@ int main()
         return 1;
     }
 
+    unsigned char *map_palettes =
+        malloc(MAP_WIDTH * MAP_HEIGHT * PALETTE_BYTES);
+
+    if (map_palettes == NULL) {
+        perror("Error allocating memory for map palettes");
+        return 1;
+    }
+
     for (int y = 0; y < MAP_WIDTH; ++y) {
         for (int x = 0; x < MAP_HEIGHT; ++x) {
             if (draw_room(x, y)) return 1;
+
+            memcpy(map_palettes + (y * MAP_WIDTH + x) * PALETTE_BYTES,
+                   rgb_palette, PALETTE_BYTES);
 
             if (write_room_pixels_to_map(map_pixels, x, y)) return 1;
         }
     }
 
-    return write_map_pixels_to_image(map_pixels);
+    return write_map_pixels_to_image(map_pixels, map_palettes);
 }
 
 int write_room_pixels_to_map(unsigned char *map_pixels, int map_x, int map_y)
@@ -54,7 +67,8 @@ int write_room_pixels_to_map(unsigned char *map_pixels, int map_x, int map_y)
     return 0;
 }
 
-int write_map_pixels_to_image(unsigned char *map_pixels)
+int write_map_pixels_to_image(unsigned char *map_pixels,
+                              unsigned char *map_palettes)
 {
     unsigned char *rgb =
         (unsigned char *)malloc(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
@@ -67,11 +81,15 @@ int write_map_pixels_to_image(unsigned char *map_pixels)
 
             int rgb_index = (y * 3 * IMAGE_WIDTH) + (x * 3);
 
+            unsigned char *palette =
+                map_palettes + (y / SCREEN_HEIGHT) * MAP_WIDTH * PALETTE_BYTES +
+                (x / SCREEN_WIDTH) * PALETTE_BYTES;
+
             int palette_index = pixel * 4;
 
-            rgb[rgb_index] = rgb_palette[palette_index + 2];     // R
-            rgb[rgb_index + 1] = rgb_palette[palette_index + 1]; // G
-            rgb[rgb_index + 2] = rgb_palette[palette_index];     // B
+            rgb[rgb_index] = palette[palette_index + 2];     // R
+            rgb[rgb_index + 1] = palette[palette_index + 1]; // G
+            rgb[rgb_index + 2] = palette[palette_index];     // B
         }
     }
 
